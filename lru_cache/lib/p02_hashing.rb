dca6f3bac2a615425aab2f12c91de35b52104a82
Timmy Jing <timmyjing@gmail.com>
2018-08-07 00:37:47 -0700

    Finished hashing

--- lru_cache/lib/p02_hashing.rb
+++ lru_cache/lib/p02_hashing.rb
@@ -4,11 +4,14 @@ end
 
 class Array
   def hash
+    map.with_index {|el, i| el.hash * i.hash}.inject(0, :+)
   end
 end
 
 class String
   def hash
+    return ord.hash if length <= 1
+    return chars.hash if length > 1
   end
 end
 
@@ -16,5 +19,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
+    keys.sort.hash + values.sort.hash
   end
 end
